577ca587ab05264db664c772799cc576c7158d79
James Dick SE2018 <qsb19184@telford-04.cis.strath.ac.uk>
2020-01-30 12:19:21 +0000

    Made it so the program exits on ctrl+D

--- shell.c
+++ shell.c
@@ -3,14 +3,18 @@
 #include <string.h>
 
 int main() {
-    char buffer[512];
-    while (strncmp(buffer, "exit", 4)) {
-        printf("$ ");
-        fgets(buffer, 512, stdin);
+
+    char buffer[513];
+    printf("$ ");
+    fgets(buffer, 512, stdin);
+    while (strncmp(buffer, "exit", 4) && !feof(stdin)) {
+        
         char* token = strtok(buffer, " ");
         while (token != NULL) {
             printf("arg: %s\n", token);
             token = strtok(NULL, " ");
         }
+        printf("$ ");
+        fgets(buffer, 512, stdin);
     }
 }
