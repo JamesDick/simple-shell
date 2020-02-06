02956e7785612a634ce9c571587392c6de7fce27
James Dick SE2018 <qsb19184@telford-04.cis.strath.ac.uk>
2020-02-06 11:31:23 +0000

    Changed the program to add each token to an argument array instead of just printing them

--- shell.c
+++ shell.c
@@ -8,12 +8,14 @@ int main() {
     // Print to console and prompt user to enter a value
     printf("$ ");
     fgets(user_input, sizeof(user_input), stdin);
-    // Keep asking the user to enter a value (except when they enter 'exit')
+    // Keep asking the user to enter a value (except when they enter 'exit' or CTRL+D)
     while (strncmp(user_input, "exit", 4) && !feof(stdin)) {
         // Split the user input into words
         char* token = strtok(user_input, " \n\t|><&;");
+        char* args[50];
+        int argCount = 0;
         while (token != NULL) {
-            printf("'%s'\n", token);
+            args[argCount++] = token;
             token = strtok(NULL, " \n\t|><&;");
         }
         // Prompt for the next user value
