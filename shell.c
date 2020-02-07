31696455c206fec174b1387e98ee80b7c8a7d936
James Dick <slightlyjames@gmail.com>
2020-02-07 16:50:20 +0000

    Revert "GetFuncy"
    This reverts commit a59bd9b24d89f1611a04c8763eaa90db685438bb.

--- shell.c
+++ shell.c
@@ -5,53 +5,46 @@
 #include<sys/wait.h> 
 #include <unistd.h> 
 
-void get_input(char* user_input);
-void get_args(char** args, char* user_input);
-void exec_cmd(char** args);
-
 int main() {
-    // Declare a buffer to store the user input and an array to hold the arguments
+    // Declare a buffer to store the user input
     char user_input[512];
-    char* args[50];
-    get_input(user_input);
-
-    while (strncmp(user_input, "exit", 4) && !feof(stdin)) {
-        get_args(args, user_input);
-        exec_cmd(args);    
-        get_input(user_input);
-    }
-}
-
-void get_input(char* user_input) {
+    // Print to console and prompt user to enter a value
     printf("$ ");
     fgets(user_input, sizeof(user_input), stdin);
-}
-
-void get_args(char** args, char* user_input) {
-    char* token = strtok(user_input, " \n\t|><&;");
-    int arg_count = 0;
-    while (token != NULL) {
-        args[arg_count++] = token;
-        token = strtok(NULL, " \n\t|><&;");
-    }
-    args[arg_count] = NULL;
-}
-
-void exec_cmd(char** args) {
-    pid_t c_pid, pid;
-    int status;
-    c_pid = fork();
-    if (c_pid == -1) {
-        perror("Error: Fork failed!\n");
-        _exit(1);
-    }
-    if (c_pid == 0) {
-        execvp(args[0], args);
-        perror("");
-    }
-    else if (c_pid > 0){
-        if((pid = wait(&status)) < 0) {
+    // Keep asking the user to enter a value (except when they enter 'exit' or CTRL+D)
+    while (strncmp(user_input, "exit", 4) && !feof(stdin)) {
+        // Split the user input into words
+        char* token = strtok(user_input, " \n\t|><&;");
+        char* args[50];
+        int argCount = 0;
+        pid_t c_pid, pid;
+        int status;
+        while (token != NULL) {
+            args[argCount] = token;
+            token = strtok(NULL, " \n\t|><&;");
+            argCount++;
+        }
+        args[argCount] = NULL;
+        c_pid = fork();
+        if (c_pid == -1) {
+            perror("Error: Fork failed!\n");
             _exit(1);
         }
-    } 
+        if (c_pid == 0) {
+            execvp(args[0], args);
+            perror("Error: execvp failed!\n");
+            _exit(1);
+        }
+        else if (c_pid > 0){
+            if((pid = wait(&status)) < 0) {
+                perror("Error: Wait failed!\n");
+                _exit(1);
+            }
+        }
+        // Prompt for the next user value
+        printf("$ ");
+        fgets(user_input, sizeof(user_input), stdin);
+    }
+    // Create a new line
+    printf("\n");
 }
