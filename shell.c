e90ac840f928ccbb1240e32e83b384f156da8d31
James Dick <slightlyjames@gmail.com>
2020-03-05 13:48:03 +0000

    Added a header file for shell

--- shell.c
+++ shell.c
@@ -1,18 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h> 
-#include <sys/wait.h> 
-#include <unistd.h>
-#include "history.h"
 
-#define BUFFER_SIZE 512
-#define ARG_LIMIT 50
-
-char* get_input(char* user_input);
-void get_args(char** args, char* user_input);
-void exec_cmd(char** args);
-void invoke_history(History* history, char* user_input);
+#include "shell.h"
 
 int main() {
     char user_input[BUFFER_SIZE];
