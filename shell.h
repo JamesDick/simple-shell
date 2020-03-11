a71cba0b8644ea983c1b57e80ab9825a96ecd0d9
Yohanes Zhelezov SE2018 <yohanes.zhelezov.2018@uni.strath.ac.uk>
2020-03-11 15:59:12 +0000

    Update declarations and added new defines

--- shell.h
+++ shell.h
@@ -9,11 +9,12 @@
 #include <errno.h>
 #include "history.h"
 
+#define TOO_FEW_ARGS "Too few arguments: "
+#define TOO_MANY_ARGS "Too many arguments: "
 #define BUFFER_SIZE 512
 #define ARG_LIMIT 50
 
 char* get_input(char* user_input);
-char* get_parent_dir();
 char* get_last_word(char* user_input);
 char* replacetilde(char* path);
 void display_prompt();
@@ -22,8 +23,7 @@ void get_args(char** args, char* user_input);
 void exec_cmd(char** args, bool stop);
 void get_new_path(char* user_input);
 void display_path();
-void set_dir_to_parent();
-void set_dir_to_user_input(char* user_input);
+void set_dir(char* user_input);
 int set_new_path(char* new_path);
 int split_path(char** args, char* user_input);
 bool handle_cmd(char** args, History* history);
